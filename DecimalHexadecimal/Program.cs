19b669e8ca64eb18b3ee542d925e934f4b23af39
Vladislav Vladovski <garbage@mail.bg>
2022-10-10 08:14:27 +0300

    end

--- DecimalHexadecimal/Program.cs
+++ DecimalHexadecimal/Program.cs
@@ -9,29 +9,10 @@ const string fileName = "rfid.txt";
 string input = string.Empty;
 StringBuilder sb = new();
 
-var taskKeys = new Task(ReadKeys);
-var taskProcessFiles = new Task(ProcessFiles);
-
-taskKeys.Start();
-taskProcessFiles.Start();
-
-var tasks = new[] { taskKeys };
-Task.WaitAll(tasks);
-
 do
 {
     Console.Write("Enter rfid card fob: ");
-    input = Console.ReadLine();
-
-    if (Console.ReadKey(true).Key == ConsoleKey.Escape)
-    {
-        Environment.Exit(0);
-    }
-
-    if (input == "EXIT")
-    {
-        Environment.Exit(0);
-    }
+    input = Console.ReadLine();   
 
     try
     {
@@ -68,76 +49,14 @@ do
     Console.WriteLine(sb);
     sb.Clear();
 
-    Console.WriteLine($"To Quit Enter: EXIT or pres Escape \r\n");
+    Console.WriteLine($"To quit pres Escape or any key to continue\r\n");
 
+    if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+    {
+        Environment.Exit(0);
+    }
 
 } while (true);
 
 
 Environment.Exit(0);
-
-static void ReadKeys()
-{
-    ConsoleKeyInfo key = new ConsoleKeyInfo();
-
-    while (!Console.KeyAvailable && key.Key != ConsoleKey.Escape)
-    {
-
-        key = Console.ReadKey(true);
-
-        if (key.Key == ConsoleKey.Escape) Environment.Exit(0);
-    }
-}
-
-static void ProcessFiles()
-{
-    var files = Enumerable.Range(1, 100).Select(n => "File" + n + ".txt");
-
-    var taskBusy = new Task(BusyIndicator);
-    taskBusy.Start();
-
-    foreach (var file in files)
-    {
-        Thread.Sleep(1000);
-        Console.WriteLine("Procesing file {0}", file);
-    }
-
-}
-
-static void BusyIndicator()
-{
-    var busy = new ConsoleBusyIndicator();
-    busy.UpdateProgress();
-}
-
-internal class ConsoleBusyIndicator
-{
-    int _currentBusySymbol;
-
-    public char[] BusySymbols { get; set; }
-
-    public ConsoleBusyIndicator()
-    {
-        BusySymbols = new[] { '|', '/', '-', '\\' };
-    }
-    public void UpdateProgress()
-    {
-        while (true)
-        {
-            Thread.Sleep(100);
-            var originalX = Console.CursorLeft;
-            var originalY = Console.CursorTop;
-
-            Console.Write(BusySymbols[_currentBusySymbol]);
-
-            _currentBusySymbol++;
-
-            if (_currentBusySymbol == BusySymbols.Length)
-            {
-                _currentBusySymbol = 0;
-            }
-
-            Console.SetCursorPosition(originalX, originalY);
-        }
-    }
-}
